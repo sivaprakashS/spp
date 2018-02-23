f213173c221692d8e9ab46b6aa49989b097dcc38
sivaprakashS <31438994+sivaprakashS@users.noreply.github.com>
2018-02-23 16:00:10 +0530

    lowhigh.c

---
+++ lowhigh.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,i;
+printf("enter the value");
+scanf("%d",&a);
+for(i=1;i<a;i++)
+{
+printf("%d",i\n);
+}
+if(a/2!=0)
+{
+printf("the average value is:");
+}
+getch()
+}
